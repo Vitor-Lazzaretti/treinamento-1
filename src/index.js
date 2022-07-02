ccb101a8a9128df1b55ed9bf384866b6427c6bed
Vitor Lazzaretti <vitorlazzaretti@gmail.com>
2022-07-02 13:39:13 -0300

    Router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from './About';
+import NotFound from './NotFound';
 import reportWebVitals from './reportWebVitals';
 import './config/firebase';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
